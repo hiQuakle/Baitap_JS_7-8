50d16bae3465e980100bf1fdab2a10a3396987bd
hiQuakle <thang1804ab@gmail.com>
2024-09-01 12:03:52 +0700

    update Exercise_7-8

---
+++ index.js
@@ -0,0 +1,124 @@
+let arrNumber = [];
+document.querySelector(".btn--primary").onclick = function () {
+  let number = document.getElementById("inputNum").value;
+  if (number == false) {
+    arrNumber.push((number = 0));
+  } else {
+    arrNumber.push(number);
+  }
+  document.getElementById("txtArray").innerHTML = arrNumber;
+};
+
+document.getElementById("sumDuong").onclick = function () {
+  let sum = 0;
+  for (let i = 0; i < arrNumber.length; i++) {
+    if (arrNumber[i] > 0) {
+      sum += arrNumber[i] * 1;
+    } else {
+      continue;
+    }
+  }
+  document.getElementById("txtSum").innerHTML = `Tổng số dương là: ${sum}.`;
+};
+
+document.getElementById("countNumber").onclick = function () {
+  let count = 0;
+  for (let i = 0; i < arrNumber.length; i++) {
+    if (arrNumber[i] > 0) {
+      count++;
+    }
+  }
+  document.getElementById(
+    "txtCount"
+  ).innerHTML = `Ta đếm được ${count} số dương.`;
+};
+
+document.getElementById("smallestNumber").onclick = function () {
+  let smallestNumber = arrNumber[0];
+  for (let i = 0; i < arrNumber.length; i++) {
+    if (arrNumber[i] < smallestNumber) {
+      smallestNumber = arrNumber[i];
+    }
+  }
+  document.getElementById(
+    "txtMin"
+  ).innerHTML = `Số nhỏ nhất là : ${smallestNumber}.`;
+};
+
+document.getElementById("smallestPositiveNumber").onclick = function () {
+  let arrPositiveNumber = [];
+  for (let i = 0; i < arrNumber.length; i++) {
+    if (arrNumber[i] > 0) {
+      arrPositiveNumber.push(arrNumber[i]);
+    }
+  }
+  console.log(arrPositiveNumber);
+
+  let checkArrNumber = arrPositiveNumber[0];
+  for (let z = 0; z < arrPositiveNumber.length; z++) {
+    if (arrPositiveNumber[z] < checkArrNumber) {
+      checkArrNumber = arrPositiveNumber[z];
+    }
+  }
+
+  if (arrNumber == false) {
+    document.getElementById("txtMinPos").innerHTML = `Không có số dương`;
+  } else {
+    document.getElementById(
+      "txtMinPos"
+    ).innerHTML = `Số Dương Nhỏ Nhất Là: ${checkArrNumber}`;
+  }
+};
+
+document.getElementById("soChan").onclick = function () {
+  let soChanCuoiCung;
+  for (let i = 0; i < arrNumber.length; i++) {
+    if (arrNumber[i] % 2 == 0) {
+      soChanCuoiCung = arrNumber[i];
+    }
+  }
+  document.getElementById(
+    "txtEven"
+  ).innerHTML = `Số chẵn cuối cùng là : ${soChanCuoiCung}`;
+};
+
+document.getElementById("changePos").onclick = function () {
+  let input1 = document.getElementById("inputIndex1").value;
+  let input2 = document.getElementById("inputIndex2").value;
+  let bienTam;
+
+  bienTam = arrNumber[input1];
+  arrNumber[input1] = arrNumber[input2];
+  arrNumber[input2] = bienTam;
+
+  document.getElementById(
+    "txtChangePos"
+  ).innerHTML = `Mảng sau khi đổi là:${arrNumber}`;
+};
+
+document.getElementById("minToMax").onclick = function () {
+  arrNumber.sort((a, b) => a - b);
+  document.getElementById(
+    "txtIncrease"
+  ).innerHTML = `Mảng có số từ nhỏ đến lớn: ${arrNumber}`;
+};
+
+document.getElementById("soNguyenTo").onclick = function () {
+  let firstPrimeNumber = [];
+  for (let i = 2; i < arrNumber.length; i++) {
+    if (isPrime(arrNumber[i])) {
+      firstPrimeNumber.push(arrNumber[i]);
+      break;
+    }
+  }
+  document.getElementById(
+    "txtPrime"
+  ).innerHTML = `Số nguyên tố đầu tiên trong mảng là: ${firstPrimeNumber}`;
+};
+function isPrime(arrNumber) {
+  if (arrNumber <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(arrNumber[i]); i++) {
+    if (arrNumber[i] % i === 0) return false; // Nếu chia hết cho i thì không phải số nguyên tố
+  }
+  return true;
+}
